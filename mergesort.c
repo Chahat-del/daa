fc169e624af20998f9a5abb602b5fd9f59014b30
Chahat Khandelwal <chahatkhandelwal2005@gmail.com>
2026-05-19 23:47:08 +0530

    mergesort

---
+++ mergesort.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int b[50000];
+
+void Merge(int a[], int low, int mid, int high)
+{
+    int i, j, k;
+
+    i = low;
+    j = mid + 1;
+    k = low;
+
+    while(i <= mid && j <= high)
+    {
+        if(a[i] <= a[j])
+            b[k++] = a[i++];
+        else
+            b[k++] = a[j++];
+    }
+
+    while(i <= mid)
+        b[k++] = a[i++];
+
+    while(j <= high)
+        b[k++] = a[j++];
+
+    for(k = low; k <= high; k++)
+        a[k] = b[k];
+}
+
+void MergeSort(int a[], int low, int high)
+{
+    int mid;
+
+    if(low >= high)
+        return;
+
+    mid = (low + high) / 2;
+
+    MergeSort(a, low, mid);
+
+    MergeSort(a, mid + 1, high);
+
+    Merge(a, low, mid, high);
+}
+
+int main()
+{
+    int n, a[50000], k;
+
+    clock_t st, et;
+
+    double ts;
+
+    printf("Enter the number of elements\n");
+
+    scanf("%d", &n);
+
+    printf("The elements to array using Random Generator\n");
+
+    for(k = 0; k < n; k++)
+    {
+        a[k] = rand() % 1000;
+    }
+
+    st = clock();
+
+    MergeSort(a, 0, n - 1);
+
+    et = clock();
+
+    ts = ((double)(et - st) * 1000) / CLOCKS_PER_SEC;
+
+    printf("Time taken is %lf", ts);
+
+    return 0;
+}
